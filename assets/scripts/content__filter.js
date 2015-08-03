02565ad3f225abd8276f20516f245bf486f50ce3
Sergio <s-kobets617@yandex.ru>
2015-08-03 13:51:18 +0300

    sdelal filtr po stranam

--- assets/scripts/content__filter.js
+++ assets/scripts/content__filter.js
@@ -1,7 +1,47 @@
+$('.countries__content__filter').on('click', 'li', function(){
+	$(".countries__content__filter li").removeClass("activ"); //удаляем класс во всех вкладках
+	$(this).addClass("activ"); //добавляем класс текущей (нажатой)
+	});
 
-
-
-$('li.culture').on('click', function(){
-	$('.countries__content__blocks__item__norway').hide();
-	$('.countries__content__blocks__item__norway_culture').show();
+var noCulture=$('div.noCulture');
+var culture=$('div.culture');
+var disable=$('div.disable');
+var exstreme=$('div.exstreme');
+var noExstreme=$('.culture.noExtreme');
+var atmosphere=$('div.atmosphere');
+var noAtmosphere=$('.culture.noAtmosphere');
+var people=$('div.people');
+var noPeople=$('div.noPeople');
+$('li.all').on('click', function(){
+	culture.show();
+	noCulture.show();
+	disable.hide();	
+	noExstreme.hide();
+	noAtmosphere.hide();	
+});
+$('li.culture').on('click', function(){	
+	noCulture.hide();
+	culture.show();
+	disable.hide();
+});
+$('li.exstreme').on('click', function(){
+	culture.hide();
+	noCulture.hide();
+	disable.show();
+	exstreme.show();
+	noExstreme.show();	    	 
+});	
+$('li.atmosphere').on('click', function(){	
+    culture.hide();
+	noCulture.hide();
+	disable.show();
+	atmosphere.show();
+	noAtmosphere.show();	
+});
+$('li.people').on('click', function(){	
+    culture.hide();
+    noCulture.hide();
+    disable.show();
+	noPeople.show();	
+    people.show();
 });
