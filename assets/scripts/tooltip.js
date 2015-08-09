6895a8126ab57ccd84ca114b9036035ae685bef8
Siarhey <s-kobets617@yandex.ru>
2015-08-09 15:44:46 +0300

    podskazki

---
+++ assets/scripts/tooltip.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+var j = $(".tooltip:first").css("display");
+
+ $("#open_tooltip").on('click', function(){ 
+$(".tooltip").toggle("slow");
+
+
+
+/*if (d==='displa'){
+	$("#open_tooltip").text('Открыть Подсказки');
+}
+else {$("#open_tooltip").text('Cкрыть Подсказки');}
+*/
+ });
+console.log("j");
+)};
